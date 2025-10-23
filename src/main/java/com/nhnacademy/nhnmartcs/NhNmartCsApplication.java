15f7ec97ed9b39a46337122aaaa34beda0e63b3c
eugene <hash_ff0000@naver.com>
2025-10-23 10:01:38 +0900

    init commit

---
+++ src/main/java/com/nhnacademy/nhnmartcs/NhNmartCsApplication.java
@@ -0,0 +1,13 @@
+package com.nhnacademy.nhnmartcs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NhNmartCsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NhNmartCsApplication.class, args);
+    }
+
+}
